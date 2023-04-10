2524f61a1397d92cc3140994e7f57445ccafabbd
Daniel Lee <dlee923@gmail.com>
2023-04-10 11:43:09 -0400

    installed multer via npm

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', function(req, res) {
+  let filePropertiesObj = {
+    name: 'filename',
+    type: 'filetype',
+    size: 'filesize'
+  }
+  res.json(filePropertiesObj);
+});
 
 
 const port = process.env.PORT || 3000;
